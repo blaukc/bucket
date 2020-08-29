b0e571f550f5d6612343a55bc0cb150b212794c2
blaukc <brandonlau1@hotmail.com>
2020-08-30 00:12:16 +0800

    Updates

---
+++ precache-manifest.6dac65413e58e28d349e0536c3424ce2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f3487f47bd1a971ae2a8a1dc8ecc330",
+    "url": "/bucket/index.html"
+  },
+  {
+    "revision": "b0bc859f895acf212bf5",
+    "url": "/bucket/static/js/2.0382a6f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bucket/static/js/2.0382a6f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a359a3eb60770ce386ee",
+    "url": "/bucket/static/js/main.43c96063.chunk.js"
+  },
+  {
+    "revision": "9489a2fbf16799423589",
+    "url": "/bucket/static/js/runtime-main.7f1a1fcc.js"
+  }
+]);
